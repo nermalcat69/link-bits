ad9ce869cb43f3d7b4f4c8420be51b57f6b86a12
Nermal <nermalcat69@skiff.com>
2023-11-01 14:53:47 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# SvelteKit Template
+This template comes with: 
+- SvelteKit
+- Typescript
+- TailwindCSS
+- Vite
+
+To use this repo 
+1. Click the "use this template" button at the top of the page
+2. Install node.js and run the `npm i` command in the console once you have the repo open in an editor or terminal 
+3. Update the project name in the package-lock.json and package.json files
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { time } from '../lib/stores';
+
+	const formatter = new Intl.DateTimeFormat(
+		'en',
+		{
+			hour12: true,
+			hour: 'numeric',
+			minute: '2-digit',
+			second: '2-digit'
+		}
+	);
+</script>
+
+<h1 class="text-5xl text-center font-medium text-neutral-200">The time is {formatter.format($time)}</h1>
+
+<style lang="postcss">
+:global(html) {
+    background-color: theme(colors.neutral.900);
+
+    
+}
+</style>
